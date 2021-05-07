27f7e8985c54d653e25bcce8cf56a36365a12b6a
Tanner Stocks <tannstocks@gmail.com>
2021-05-06 18:16:38 -0600

    lots

--- RoboticArm.ino
+++ RoboticArm.ino
@@ -4,43 +4,41 @@
 #include <RF24.h>
 
 
-RF24 radio(6, 7);
+RF24 radio(2, 3);
 const byte address[] = "00001";
 
 
 void setup() {
     Serial.begin(9600);
     
-    radio.begin();
+    Serial.println(radio.begin());
     radio.openReadingPipe(0, address);
     radio.startListening();
 
     RoboticArm::Setup();
 }
 
-
-void loop() {
-    while(radio.available())  {
-      
+void CheckRadio() {
+  if(radio.available())  {
         char payload[] = "";
         uint8_t payloadSize = radio.getPayloadSize(); 
         radio.read(&payload, payloadSize);
         String payloadString = String(payload);
 
         int startIndex = payloadString.indexOf('<');                //Parsing a string is apparently terrible in C++
-        if( startIndex < 0) continue;
+        if( startIndex < 0) return;
         int comma1Index = payloadString.indexOf(',', startIndex+1);
-        if( comma1Index < 0) continue;
+        if( comma1Index < 0) return;
         int comma2Index = payloadString.indexOf(',', comma1Index+1);
-        if( comma2Index < 0) continue;
+        if( comma2Index < 0) return;
         int comma3Index = payloadString.indexOf(',', comma2Index+1);
-        if( comma3Index < 0) continue;
+        if( comma3Index < 0) return;
         int comma4Index = payloadString.indexOf(',', comma3Index+1);
-        if( comma4Index < 0) continue;
+        if( comma4Index < 0) return;
         int comma5Index = payloadString.indexOf(',', comma4Index+1);
-        if( comma5Index < 0) continue;
+        if( comma5Index < 0) return;
         int endIndex = payloadString.indexOf('>', comma5Index+1);
-        if( endIndex < 0) continue;
+        if( endIndex < 0) return;
     
         int baseAngle = payloadString.substring(startIndex+1, comma1Index).toInt();
         int shoulderAngle = payloadString.substring(comma1Index+1, comma2Index).toInt();
@@ -54,7 +52,8 @@ void loop() {
         Serial.print(elbowAngle);
         Serial.print(wristAngle);
         Serial.print(forearmAngle);
-        Serial.print(clawAngle);
+        Serial.println(clawAngle);
+        //Serial.println(payloadString);
         
         RoboticArm::SetBaseAngle(baseAngle);
         RoboticArm::SetShoulderAngle(shoulderAngle);
@@ -63,6 +62,11 @@ void loop() {
         RoboticArm::SetForearmAngle(forearmAngle);
         RoboticArm::SetClawAngle(clawAngle);
     }
+}
 
+
+void loop() {
+    CheckRadio();
     RoboticArm::UpdateArmPosition();
+    delay(100);
 }
